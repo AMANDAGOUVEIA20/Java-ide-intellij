d460deeeb773ac8f1c4eb6595bbb544604ee8495
Amanda Gouveia <amandacosta2299@gmail.com>
2022-07-25 16:23:49 -0300

    criando class Livro

--- src/br/com/dio/PrimeiroProgramaJava.java
+++ src/br/com/dio/PrimeiroProgramaJava.java
@@ -7,8 +7,44 @@ public class PrimeiroProgramaJava {
         Gato gato = new Gato();
         System.out.println(gato);
 
+        Livro livro = new Livro("O pequeno príncipe", 250);
+        System.out.println(livro);
+
         /*int a = 5;
         int b = 6;
         System.out.println("Hello Java! " + (a+b));*/
+    }
+}
+class Livro{
+    private String nome;
+    private String numpag;
+
+    public Livro(String nome, int numpag) {
+        this.nome = nome;
+        this.numpag = String.valueOf(numpag);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumpag() {
+        return numpag;
+    }
+
+    public void setNumpag(String numpag) {
+        this.numpag = numpag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numpag='" + numpag + '\'' +
+                '}';
     }
 }
